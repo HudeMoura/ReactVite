4403f3f06d97254621bc9486bc2727f4cb2f4ef7
HudeMoura <felipemoura92@gmail.com>
2023-08-20 00:26:18 -0300

    feat: create login component

--- ReactVite/src/modules/login/screens/LoginScreen.tsx
+++ ReactVite/src/modules/login/screens/LoginScreen.tsx
@@ -1,10 +1,14 @@
-import { BackgroundImage } from "../styles/loginScreen.styles";
+import { BackgroundImage, ContainerLogin, LogoImage, LimitedContainer, ContainerLoginScreen } from "../styles/loginScreen.styles";
 
 const LoginScreen = () => {
 
-    return (<div>
+    return (<ContainerLoginScreen>
         <BackgroundImage src="./background.png"/>
-    </div>)
+        <ContainerLogin>
+            <LimitedContainer />
+            <LogoImage src="./logo.png" />
+        </ContainerLogin>
+    </ContainerLoginScreen>)
 }
 
 export default LoginScreen;
