6ab8c71fd140ecc610f7fca0d6cadee901dbe1db
HudeMoura <felipemoura92@gmail.com>
2023-09-06 10:34:45 -0300

    feat: create first global component

--- ReactVite/src/modules/login/screens/LoginScreen.tsx
+++ ReactVite/src/modules/login/screens/LoginScreen.tsx
@@ -1,14 +1,25 @@
-import { BackgroundImage, ContainerLogin, LogoImage, LimitedContainer, ContainerLoginScreen } from "../styles/loginScreen.styles";
+/* eslint-disable prettier/prettier */
+import Input from '../../../shared/inputs/input/input';
+import {
+  BackgroundImage,
+  ContainerLogin,
+  ContainerLoginScreen,
+  LimitedContainer,
+  LogoImage,
+} from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
-
-    return (<ContainerLoginScreen>
-        <BackgroundImage src="./background.png"/>
-        <ContainerLogin>
-            <LimitedContainer />
-            <LogoImage src="./logo.png" />
-        </ContainerLogin>
-    </ContainerLoginScreen>)
-}
+  return (
+    <ContainerLoginScreen>
+      <BackgroundImage src="./background.png" />
+      <ContainerLogin>
+        <LimitedContainer />
+        <LogoImage src="./logo.png" />
+        <Input title="USUÁRIO"/>
+        <Input title="SENHA"/>
+      </ContainerLogin>
+    </ContainerLoginScreen>
+  );
+};
 
 export default LoginScreen;
