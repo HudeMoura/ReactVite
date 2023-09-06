7cc4c828e0fd55c1b31283a4559b9fc880c41aea
HudeMoura <felipemoura92@gmail.com>
2023-09-06 19:28:33 -0300

    feat: insert state in login

--- ReactVite/src/modules/login/screens/LoginScreen.tsx
+++ ReactVite/src/modules/login/screens/LoginScreen.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable prettier/prettier */
+import { useState } from 'react';
+
 import Button from '../../../shared/inputs/input/buttons/button/Button';
 import Input from '../../../shared/inputs/input/input';
 import {
@@ -11,6 +13,17 @@ import {
 } from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  }
+
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  }
+  
   return (
     <ContainerLoginScreen>
       <BackgroundImage src="./background.png" />
@@ -18,8 +31,8 @@ const LoginScreen = () => {
         <LimitedContainer />
         <LogoImage src="./logo.png" />
         <TitleLogin level={2} type="secondary">LOGIN</TitleLogin>
-        <Input title="USUÁRIO"/>
-        <Input title="SENHA"/>
+        <Input title="USUÁRIO" margin='32px 0px 0px' onChange={handleUsername} value={username}/>
+        <Input type='password' title="SENHA" margin='32px 0px 0px' onChange={handlePassword} value={password}/>
         <Button type="primary" margin='64px 0px 16px 0px'>ENTRAR</Button>
       </ContainerLogin>
     </ContainerLoginScreen>
